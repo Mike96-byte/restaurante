17e96ed197cafeb794715c54be3e95a5e086f026
Miguel Becerra <miguelbecerra96@outlook.com>
2024-07-07 17:28:17 -0600

    Texto y diseño

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import plotly_express as px
 import datetime
+import time
 
 # Titulo de proyecto
-st.header("Proyecto de Restaurantes para analisis de sus ventas")
+st.header(
+    "*_Proyecto de Restaurantes para analisis de sus ventas_* :fork_and_knife: :wine_glass:", divider="violet")
+
+
+def stream_data():
+    """Nos ayudara a poner los textos para cada sección"""
+    for word in texto.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+# Primero vamos a leer los archivos y combinarlos.
 
 # Leer los archivos
 restaurants = pd.read_excel("datos_restaurante.xlsx", sheet_name="Datos")
@@ -55,34 +67,55 @@ df_rest["hora"] = df_rest["hora_de_cobro"].apply(
     lambda x: x.hour if isinstance(x, datetime.time) else None)
 df_rest["propina_total"] = df_rest["propina"]*df_rest["precio"]
 df_rest["dia_semana"] = df_rest["fecha"].dt.weekday
+# Termina analisis de datos
 
+# Empezamos con el analisis de los datos
 
-st.title("Analisis Exploratorio para Datos de un Restaurante")
+texto = """En esta ocasión vamos a hacer un primer analisis exploratorio para un local de restaurante, vamos a analizar sus ventas por día, por hora,
+sus propinas, cuales son los productos que más se venden, las categorias y las horas con más ventas. Todo esto con ayuda de los graficos."""
 
+# Boton para poner el texto.
+if st.button("Objetivo"):
+    st.write_stream(stream_data)
+
+# Titulo principal para el analisis
+st.header("Analisis Exploratorio para Datos de un Restaurante", divider="blue")
+
+# Obtenemos las primeras lineas del datafream
 st.write(df_rest.head(10))
 
-st.write("Vamos a observar la distribución de sus ventas totales a lo largo del mes")
+# Titulo para primer grafica de la distribución de ventas a lo largo del mes (del 1 al 31)
+st.subheader(
+    "Vamos a observar la distribución de sus ventas totales a lo largo del mes", divider="blue")
 
 # Crear grafico para la distribución de los días
-fig = px.histogram(df_rest, x="dia", y="venta_total", nbins=50)
+fig = px.histogram(df_rest, x="dia", y="venta_total",
+                   nbins=50, color="tipo_x", title="Histograma para distribución de ventas del mes", marginal="box", opacity=0.8)
 st.plotly_chart(fig, use_container_width=True)
 
 
 # Crear histograma para la distribución de ventas durante todo el día
-st.write("Histograma para las ventas durante el día.")
+st.subheader("Histograma para las ventas durante el día.", divider="rainbow")
 fig4 = px.histogram(df_rest, x="hora", y="venta_total",
-                    nbins=24, color=df_rest["tipo_x"])
+                    nbins=24, color="tipo_x", opacity=0.7)
 st.plotly_chart(fig4, use_container_width=True)
 
-
-st.write("Ahora vamos a ver los meseros como se comportan con las cuentas")
+# Titulo
+st.subheader(
+    "Ahora vamos a ver los meseros como se comportan con las cuentas", divider="rainbow")
 
 # Grafico para los meseros, vamos a identificar como se comportan con diferentes graficas
 
-hist_button = st.button("Construir un Histograma")
-scatter_button = st.button("Construir un grafico de dispersión")
-box_button = st.button("Construir diagrama de caja")
-bar_button = st.button("Construir grafico de barras para las propinas")
+# hist_checkbox = st.checkbox("Construir un Histograma")
+# scatter_checkbox = st.checkbox("Construir un grafico de dispersión")
+# box_checkbox = st.checkbox("Construir diagrama de caja")
+# bar_checkbox = st.checkbox("Construir grafico de barras para las propinas")
+
+graficos = st.radio("Seleccionar el grafico que deseas ver", [
+    ":rainbow[Histograma]", ":rainbow[Scatter]", ":rainbow[Boxplot]", ":rainbow[Barplot]"], captions=["Construye un Histograma",
+                                                                                                      "Construye una grafica de dispersion",
+                                                                                                      "Construye un diagrama de caja",
+                                                                                                      "Construye una grafica de barras"])
 
 venta_meseros = df_rest.groupby(["atendió", "dia"])[
     "venta_total"].sum().reset_index()
@@ -93,47 +126,49 @@ venta_ordenes = df_rest.groupby(["orden", "atendió", "dia"])[
 propinas = df_rest.groupby(["atendió", "tipo_de_cliente"])[
     "propina"].mean().reset_index()
 
-if hist_button:
-    st.write("Histograma para la ditribución de meseros por ventas totales")
-    fig1 = px.histogram(df_rest, x="atendió", y="venta_total")
-    st.plotly_chart(fig1, use_container_width=True)
 
-if box_button:
-    st.write("Diagrama de caja para meseros, de sus ventas por día")
-    fig3 = px.box(venta_meseros, x="atendió", y="venta_total")
-    st.plotly_chart(fig3)
-
-if scatter_button:
-    st.write("Diagrama de dispersión para los meseros")
-    fig2 = px.scatter(venta_ordenes, x="orden", y="precio")
-    st.plotly_chart(fig2, use_container_width=True)
-
-if bar_button:
+if graficos == ":rainbow[Barplot]":
     st.write(
         "Grafica de barras para ver las propinas promedios que reciben los meseros")
     fig8 = px.bar(propinas, x="atendió", y="propina",
                   color=propinas["tipo_de_cliente"])
     st.plotly_chart(fig8, use_container_width=True)
 
+elif graficos == ":rainbow[Histograma]":
+    st.write("Histograma para la ditribución de meseros por ventas totales")
+    fig1 = px.histogram(df_rest, x="atendió",
+                        y="venta_total", color="categoria")
+    st.plotly_chart(fig1, use_container_width=True)
 
-st.header(
-    "Ahora vamos a evaluar cuales son las categorias, alimentos y más que se venden")
+elif graficos == ":rainbow[Boxplot]":
+    st.write("Diagrama de caja para meseros, de sus ventas por día")
+    fig3 = px.box(venta_meseros, x="atendió", y="venta_total")
+    st.plotly_chart(fig3)
+
+else:
+    st.write("Diagrama de dispersión para los meseros")
+    fig2 = px.scatter(venta_ordenes, x="orden", y="precio", color="atendió")
+    st.plotly_chart(fig2, use_container_width=True)
+
+
+st.subheader(
+    "Ahora vamos a evaluar cuales son las categorias, alimentos y más que se venden", divider="rainbow")
 
 categorias = df_rest.groupby(["categoria", "tipo_x", "tipo_y", "atendió"])[
     "tipo_de_cliente"].size().reset_index()
 
 meseros = df_rest["atendió"].unique()
 
-fig5 = px.bar(categorias, x="categoria", y="tipo_de_cliente",
-              title="Categorias más vendidas")
+fig5 = px.bar(categorias, x="tipo_y", y="tipo_de_cliente",
+              title="Categorias más vendidas", color="categoria")
 st.plotly_chart(fig5, use_container_width=True)
 
-meseros_categoria = df_rest.groupby(["atendió", "categoria"])[
-    "tipo_de_cliente"].size().reset_index()
+meseros_categoria = df_rest.groupby(["atendió", "categoria", "tipo_x"])[
+    "venta_total"].sum().reset_index()
 
 
-fig6 = px.bar(meseros_categoria, x="atendió", y="tipo_de_cliente",
-              color=meseros_categoria["categoria"])
+fig6 = px.bar(meseros_categoria, x="atendió", y="venta_total",
+              color="tipo_x")
 st.plotly_chart(fig6)
 
 venta_categorias = df_rest.groupby([
@@ -142,6 +177,12 @@ venta_categorias = df_rest.groupby([
 fig7 = px.funnel(venta_categorias, x="venta_total",
                  y="categoria", color=venta_categorias["tipo_de_cliente"])
 st.plotly_chart(fig7)
+
+
+fig17 = px.pie(df_rest, "tipo_x", values="venta_total",
+               facet_col="tipo_de_cliente")
+st.plotly_chart(fig17)
+
 
 st.title("Vamos a hacer otro analisis exploratorio con graficas de barras")
 
@@ -153,7 +194,6 @@ fig10 = plt.figure(figsize=(10, 6))
 
 plt.bar(venta_producto["producto"], venta_producto["venta_total"])
 labels = venta_producto["producto"].unique()
-
 plt.xticks(venta_producto["producto"], labels, rotation="vertical")
 st.pyplot(fig10)
 
